95e234fa8008adb4989adb7c56b2be9b11ad679b
Cent Chan <best66@foxmail.com>
2021-12-06 11:21:51 +0800

    reactivity

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -5,10 +5,23 @@ export function effect(fn, options: any = {}) {
 
 // 用栈解决嵌套 effect 问题
 const effectStack = [];
+let activeEffect;
+let id = 0;
 
 function createReactiveEffect(fn, options) {
   const effect = function reactiveEffect() {
-    console.log(1);
+    try {
+      effectStack.push(fn);
+      activeEffect = effect;
+      return fn();
+    } finally {
+      effectStack.pop();
+      activeEffect = effectStack[effectStack.length - 1];
+    }
   };
+  effect.id = id++;
+  effect.__isEffect = true;
+  effect.options = options;
+  effect.deps = [];
   return effect;
 }
